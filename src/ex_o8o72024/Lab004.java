c1c1c4f2c326e85b7c8e1a94cbaf8c03382348ad
SujataGutte <omgutte22@gmail.com>
2024-08-08 01:42:51 -0700

    all file added

---
+++ src/ex_o8o72024/Lab004.java
@@ -0,0 +1,15 @@
+package ex_o8o72024;
+
+public class Lab004 {
+
+    public static void main(String[] args) {
+        System.out.println("Hell0");
+    }
+
+    public static void main() {
+        System.out.println("second step");
+    }
+    public static void main(int [] args) {
+        System.out.println("second step");
+    }
+}
